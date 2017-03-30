8c7be3a21b7b8a066585b1da7abc77979ab29359
amaksimov <andrei.maksimov@cbsinteractive.com>
2017-03-30 19:04:39 +0300

    second

--- src/main/java/ru/apache_maven/App.java
+++ src/main/java/ru/apache_maven/App.java
@@ -1,5 +1,9 @@
 package ru.apache_maven;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,15 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        UserDetails userDetails = new UserDetails();
+        userDetails.setUserId(3);
+        userDetails.setUserName("Third");
+
+        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        session.save(userDetails);
+        session.getTransaction().commit();
+
     }
 }
